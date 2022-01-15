f26dc7ef4c00ec8ef7d55eb0703ad609ff2c42b6
Caio Vinicius <caiovi98@hotmail.com>
2022-01-15 12:10:02 -0300

    feat: criação do projeto

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Home() {
+  return (
+    <View>
+      <Text style={{color: 'red'}}>Tela Home</Text>
+    </View>
+  );
+}
+
+export default Home;
---
+++ src/pages/Movies/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Movies() {
+  return (
+    <View>
+      <Text style={{color: 'red'}}>Tela MOVIES</Text>
+    </View>
+  );
+}
+
+export default Movies;
